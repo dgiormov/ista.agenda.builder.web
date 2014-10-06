106a0bd3082f5f86c62242a4897f14b6f983f998
dgiormov <dimitar.giormov@sap.com>
2014-10-06 15:53:42 +0300

    make more and less links work

--- app/scripts/controllers/details.js
+++ app/scripts/controllers/details.js
@@ -9,6 +9,7 @@
  */
 angular.module('istaAngularApp')
 	.controller('DetailsCtrl', function($scope, $routeParams, sessionDetails, sessionComments) {
+		$scope.descrClass="description-collapsed";
 		$scope.session = sessionDetails.get({
 			id: $routeParams.id
 		});
@@ -22,5 +23,12 @@ angular.module('istaAngularApp')
 		$scope.onSave = function() {
 		      sessionComments.save({ session_id: $routeParams.id}, {data: $scope.comment});
 			  $scope.refreshComments();
-		    }
+		}
+		$scope.expandColapse = function(){
+			if($scope.descrClass ==="description-expanded"){
+				$scope.descrClass="description-collapsed";
+			} else {
+				$scope.descrClass="description-expanded";
+			}
+		}
 	});
