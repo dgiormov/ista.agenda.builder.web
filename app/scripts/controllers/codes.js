abe7ba3656fa2b9092965cf2458f2dd1cf72acef
dgiormov <dimitar.giormov@sap.com>
2014-11-20 15:48:54 +0200

    challenge pictures and fixes

--- app/scripts/controllers/codes.js
+++ app/scripts/controllers/codes.js
@@ -12,14 +12,26 @@ angular.module('istaAngularApp')
 	  $scope.tittle = "Unlock the challenge by reaching Gold User status !";
 	  $scope.description = "";
   	  $scope.counter = 0;
+	  $scope.imgurl = "images/challenge/baby.png";
 	  $scope.challenge = challenge.get(function(){
 		  if($scope.challenge.rank > 1){
 			  if($scope.challenge.unlocked){
-				  $scope.tittle = "Challenge unlocked";		  	
-	  	  	 	  $scope.counter = $scope.challenge.counter;
+				  if(!$scope.challenge.code){
+					  $scope.tittle = "Challenge started";
+					  $scope.imgurl = "images/challenge/charity.jpg";
+		  	  	 	  $scope.counter = $scope.challenge.counter;				  	
+				  } else {
+					  $scope.tittle = "Challenge complete";
+					  $scope.imgurl = "images/challenge/charity.jpg";
+		  	  	 	  $scope.counter = $scope.challenge.counter;				  	
+				  }
+				  if($scope.challenge.points > 200){
+					  $scope.imgurl = "images/challenge/charity.jpg";
+				  }
 			  } else {
 				  $scope.tittle = "You can unlock the challenge now.";
-  	  	 	  $scope.counter = "";
+  			  	  $scope.imgurl = "images/challenge/treasure-map.png";
+				  $scope.counter = "";
 		  		}
 		  }
 	  	
@@ -34,6 +46,8 @@ angular.module('istaAngularApp')
 	  $scope.goldAndFinished = function(){
 		  return $scope.challenge.rank > 1 && $scope.challenge.unlocked && $scope.challenge.code;
 	  }
-	  
+	  $scope.baby = function(){
+		  return $scope.challenge.rank < 2;
+	  }
 	  
   });
