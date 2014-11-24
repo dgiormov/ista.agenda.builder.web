1d7dd4851b9222f34f858bb09000863362ca78b2
dgiormov <dimitar.giormov@sap.com>
2014-11-24 20:59:21 +0200

    add stars in top session speakers

--- app/scripts/controllers/topsessions.js
+++ app/scripts/controllers/topsessions.js
@@ -11,4 +11,15 @@ angular.module('istaAngularApp')
   .controller('TopSessionCtrl', function($scope, topsession, topspeaker) {
 	  $scope.speakers = topspeaker.query({});
 	  $scope.sessions = topsession.query({});
+	  
+	  
+	  $scope.getStar = function(index, rating){
+		  if(index <= rating){
+			  return "fa-star";
+		  }
+		  if(index>rating && index-1<rating){
+			  return "fa-star-half-o";
+		  }
+		  return "fa-star-o";
+	  }
   });
