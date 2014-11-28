6b04f52814c823a06318de36c433a2121c019100
dgiormov <dimitar.giormov@sap.com>
2014-11-28 10:24:08 +0200

    remove the trash

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -58,10 +58,6 @@ angular
       .when('/partners', {
         templateUrl: 'views/partners.html'
       })
-      .when('/ranking_public', {
-        templateUrl: 'views/ranking_public.html',
-        controller: 'RankingCtrl'
-      })
       .otherwise({
         redirectTo: function() {
           // set default date
